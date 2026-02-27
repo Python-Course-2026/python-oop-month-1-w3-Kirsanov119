f18575bdcf0a1a77e661bccd8fddf91bb263a477
anton kirsanov <anton.kirsanov2017a@gmail.com>
2026-02-27 21:10:35 +0300

    Выполнил 5-7 дни

--- week_intensiv/day6_systems/tasks/battle_system.py
+++ week_intensiv/day6_systems/tasks/battle_system.py
@@ -4,16 +4,28 @@ class Hero:
         self.hp = hp
         self.atk = atk
 
+
 class Battle:
     """
-    ЗАДАЧА: Симуляция боя до смерти одного из героев.
-    1. Метод fight(h1, h2) запускает цикл: пока у обоих HP > 0.
-    2. В каждом раунде:
-       - h1 наносит урон h2 (h2.hp -= h1.atk).
-       - Если h2 еще жив (hp > 0), он бьет в ответ (h1.hp -= h2.atk).
-    3. Метод должен вернуть имя победителя (у кого осталось HP > 0).
-    4. Если оба погибли в один ход (маловероятно при такой логике, но всё же) или на входе кто-то мертв — вернуть имя выжившего.
+    Класс для симуляции боя между героями.
     """
+
     def fight(self, h1: Hero, h2: Hero):
-        # ТВОЙ КОД ЗДЕСЬ
-        pass
+        if h1.hp <= 0 and h2.hp <= 0:
+            return "Оба мертвы"
+        elif h1.hp <= 0 and h2.hp > 0:
+            return h2.name
+        elif h2.hp <= 0 and h1.hp > 0:
+            return h1.name
+
+        while h1.hp > 0 and h2.hp > 0:
+                h1.hp -= h2.atk
+                h2.hp -= h1.atk
+
+
+        if h1.hp > 0 and h2.hp <= 0:
+            return h1.name
+        elif h2.hp > 0 and h1.hp <= 0:
+            return h2.name
+        else:
+            return "Ничья"
